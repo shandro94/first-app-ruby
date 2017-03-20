d5bc7593706cb2ebf59dea06de014bb207be737b
shandro <shandro94@mail.ru>
2017-03-21 03:36:20 +0400

    added 'add to file' users, added 'message in /contacts

--- app.rb
+++ app.rb
@@ -15,6 +15,28 @@ get '/appointment' do
 	erb :appointment
 end
 
+post '/appointment' do
+	@name = params[:username]
+	@phone = params[:phone]
+	@time = params[:time]
+	@performer = params[:performer]
+
+	lines = []
+	File.open './public/users.txt','a' do |file|
+		file.write "Name: #{@name}; Phone: #{@phone}; Time: #{@time}; Performer:#{@performer}\n"
+	end
+
+	@message = 'Запись успешно произведена.'
+
+	erb :appointment
+
+end
+
 get '/contacts' do
+	erb :contacts
+end
+
+post '/contacts' do
+	@message = "Button was pressed."
 	erb :contacts
 end
