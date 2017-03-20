bce0454d046c9a5fefc2191409bb3310e63761fc
shandro <shandro94@mail.ru>
2017-03-20 21:01:01 +0400

    add sinatra-reloader

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 #encoding: utf-8
 require 'rubygems'
 require 'sinatra'
-#require 'sinatra/reloader'
+require 'sinatra/reloader' if development?
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "Hello, <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
