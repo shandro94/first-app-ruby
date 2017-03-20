a352b28ffe41da0fdf0470cdfa8b855d49f32d26
shandro <shandro94@mail.ru>
2017-03-21 00:03:58 +0400

    added appointed users

--- app.rb
+++ app.rb
@@ -6,3 +6,15 @@ require 'sinatra/reloader' if development?
 get '/' do
 	erb "Hello, <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+get '/about' do
+	erb :about
+end
+
+get '/appointment' do
+	erb :appointment
+end
+
+get '/contacts' do
+	erb :contacts
+end
