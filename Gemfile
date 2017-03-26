a2812e12b2ffcebcb077c50ec9a3ffe87a7f5546
shandro94 <shandro94@mail.ru>
2017-03-26 16:14:29 +0500

    Setted @error missings and pony

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 gem "sinatra"
 gem 'sinatra-reloader', '~> 1.0'
 gem 'sinatra-contrib', '~> 2.0.0.rc2'
+gem 'pony', '~> 1.11'
--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'pony'
+
 
 get '/' do
 	erb "Hello, <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -34,7 +36,7 @@ post '/appointment' do
 	#	end
 	#end
 
-	unless @error #if not
+	if @error == '' #if not
 		File.open './public/users.txt','a' do |file|
 			file.write "Name: #{@username}; Phone: #{@phone}; Time: #{@time}; Performer:#{@performer}\n"
 		end
@@ -48,12 +50,37 @@ get '/contacts' do
 end
 
 post '/contacts' do
+	
 	@push_notification = "The message has been successfully sent."
-	@email = params[:email]
+	@nickname = params[:nickname]
 	@message = params[:message]
 
-	
+	errors = {
+		:nickname => 'Enter your name',
+		:message => 'Enter your message',
+	}
+
+	Pony.mail({
+		:to => 'shandro94@mail.ru', 
+		:subject => @nickname + " has contacted you via the Website",
+		:body => @message,
+	    :via => :smtp,
+	    :from => "Site/contacts/#{@nickname} <shandro94@mail.ru>",
+	    :via_options => {
+	     :address              => 'smtp.mail.ru',
+	     :port                 => '587',
+	     :enable_starttls_auto => true,
+#	     :user_name            => 'ilya.lubit.tanyu@gmail.com',
+#	     :password             => 'Core2duo1',
+	     :user_name            => 'shandro94@mail.ru',
+	     :password             => 'Core2Duo1',
+	     :authentication       => :plain, 
+	     :domain               => "localhost.localdomain" 
+	     }
+    })
 
 
 	erb :contacts
+
+
 end
