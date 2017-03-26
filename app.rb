b16c5d1138a82c0516e6bdee9da64783333bfa28
shandro94 <shandro94@mail.ru>
2017-03-26 16:32:01 +0500

    Added validation for sending message via /Contacts

--- app.rb
+++ app.rb
@@ -30,11 +30,6 @@ post '/appointment' do
 	}
 
 	@error = errors.select {|key,_| params[key]==""}.values.join("<br>")
-	#errors.each do |key,err| 
-	#	if params[key] == ''
-	#		@error = errors[key]
-	#	end
-	#end
 
 	if @error == '' #if not
 		File.open './public/users.txt','a' do |file|
@@ -51,7 +46,6 @@ end
 
 post '/contacts' do
 	
-	@push_notification = "The message has been successfully sent."
 	@nickname = params[:nickname]
 	@message = params[:message]
 
@@ -60,27 +54,29 @@ post '/contacts' do
 		:message => 'Enter your message',
 	}
 
-	Pony.mail({
-		:to => 'shandro94@mail.ru', 
-		:subject => @nickname + " has contacted you via the Website",
-		:body => @message,
-	    :via => :smtp,
-	    :from => "Site/contacts/#{@nickname} <shandro94@mail.ru>",
-	    :via_options => {
-	     :address              => 'smtp.mail.ru',
-	     :port                 => '587',
-	     :enable_starttls_auto => true,
-#	     :user_name            => 'ilya.lubit.tanyu@gmail.com',
-#	     :password             => 'Core2duo1',
-	     :user_name            => 'shandro94@mail.ru',
-	     :password             => 'Core2Duo1',
-	     :authentication       => :plain, 
-	     :domain               => "localhost.localdomain" 
-	     }
-    })
 
+	@error = errors.select {|key,_| params[key]==""}.values.join("<br>")
 
+	if @error == '' #if not
+		Pony.mail({
+				:to => 'shandro94@mail.ru', 
+				:subject => @nickname + " has contacted you via the Website",
+				:body => @message,
+			    :via => :smtp,
+			    :from => "Site/contacts/#{@nickname} <shandro94@mail.ru>",
+			    :via_options => {
+			     :address              => 'smtp.mail.ru',
+			     :port                 => '587',
+			     :enable_starttls_auto => true,
+			     :user_name            => 'shandro94@mail.ru',
+			     :password             => 'Core2Duo1',
+			     :authentication       => :plain, 
+			     :domain               => "localhost.localdomain" 
+			     }
+		    })
+		@push_notification = 'The message has been sent successfully.'
+	end
+		
 	erb :contacts
-
 
 end
