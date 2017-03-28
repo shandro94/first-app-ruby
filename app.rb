1c65599490d548f48bd33bcddbeed245dca7d58c
shandro94 <shandro94@mail.ru>
2017-03-28 19:30:24 +0300

    added alert plugin

--- app.rb
+++ app.rb
@@ -21,6 +21,10 @@ get '/' do
 	erb "Hello, <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
+get '/something' do
+	erb :something
+end
+
 get '/about' do
 	erb :about
 end
